ee69ed7358d623f7d53e1f1e96aa680a284febc3
Samuel <samuel@umich.edu>
2020-05-22 22:23:45 -0400

    graph of dependencies

--- docs/get.py
+++ docs/get.py
@@ -1,18 +1,22 @@
 import glob
-file_nm = '../lambda/extract.c' 
+import numpy as np
 
 def parse(file_nm): 
     with open(file_nm) as f:
         lines = f.read().split('\n')
     
     types = [] 
+    includes = []
     parsed = [['supersection', file_nm.replace('_', '\\_')]]
     
     l = 0 
     while l != len(lines):  
         if lines[l].strip().startswith('typedef'): 
             types.append(lines[l].split(';')[0].strip().split()[-1]) 
-            print(file_nm, lines[l])
+
+        if lines[l].startswith('#include "'): 
+            #includes.append(lines[l].split()[1].replace('"', ''))
+            includes.append(lines[l].split()[1].split('/')[-1].replace('"', ''))
 
         if lines[l].strip().startswith('/*='): 
             title = ' '.join(lines[l+1].strip().split(' ')[2:-1])
@@ -31,7 +35,7 @@ def parse(file_nm):
             parsed[-1][1] += lines[l]+'\n'
         l += 1
             
-    return types, parsed
+    return types, includes, parsed
 
 def texify(tag, data):
     if tag == 'body':
@@ -48,20 +52,89 @@ def texify(tag, data):
             'subsubsection': '\\moosubsubsection{{ {} }}',
         }[tag].format(data)
 
+def make_dep_table(dep_dict):
+    new_dep_dict = {}
+    for k in dep_dict:
+        kk = k.split('.')[0]
+        if kk not in new_dep_dict:
+            new_dep_dict[kk] = set()
+        new_dep_dict[kk].update( {v.split('.')[0] for v in dep_dict[k]} )
+    dep_dict = new_dep_dict
+
+    ll = list(dep_dict.keys())
+    mm = np.array([
+        [
+            1.0 if ll[i] in dep_dict[ll[j]] else 0.0
+            for j in range(len(ll))
+        ]
+        for i in range(len(ll))
+    ]) + 10.0 * np.eye(len(ll))
+    mm3 = np.matmul(mm, np.matmul(mm, mm))
+    w, v = np.linalg.eig(mm)
+    ii = max((w[i], i) for i in range(len(w)))[1]
+    v = [vv[ii] for vv in v]
+
+    H, W = 14, 7
+    fnm_by_pos = {} 
+    pos_by_fnm = {} 
+
+    min_r = min(np.sum(mm3[:,i]) for i in range(len(ll)))
+    max_r = max(np.sum(mm3[:,i]) for i in range(len(ll)))
+    for nm in dep_dict: 
+        i = ll.index(nm)
+        r = np.random.binomial(H-1, p = max(0.1, min(0.9, (np.sum(mm3[:,i])-min_r)/(max_r-min_r))) ) 
+        c = np.random.binomial(W-1, p = max(0.1, min(0.9, (v[i]-min(v))/(max(v)-min(v)))) ) 
+        while (r, c) in fnm_by_pos: 
+            r = np.random.binomial(H-1, p = max(0.1, min(0.9, (np.sum(mm3[:,i])-min_r)/(max_r-min_r))) ) 
+            c = np.random.binomial(W-1, p = max(0.1, min(0.9, (v[i]-min(v))/(max(v)-min(v)))) ) 
+        fnm_by_pos[(r,c)] = nm
+        pos_by_fnm[nm] = (r,c)
+
+    body = ''
+    for r in range(H): 
+        for c in range(W): 
+            if (r,c) in fnm_by_pos:
+                body += '{{ {} {} }}'.format(
+                    '\\color{mylime}' if fnm_by_pos[(r,c)].startswith('test_') else '\\color{mypurp}',
+                    fnm_by_pos[(r,c)].replace('_', '\\_')
+                )
+                for incl in dep_dict[fnm_by_pos[(r,c)]]:
+                    rr, cc = pos_by_fnm[incl] 
+                    if (rr,cc)==(r,c): continue
+                    body += ' \\arrow[mygray, {}, bend right=10]'.format(
+                        'u'*(r-rr)+
+                        'd'*(rr-r)+
+                        'l'*(c-cc)+
+                        'r'*(cc-c)
+                    )
+            else:
+                body += '{}'
+            if c+1 != W: body += ' & '
+        if r+1 != H: body += ' \\\\ \n'
+    return body
+
 def do_all():
+    DEPS = {}
     TYPES = [] 
     CODE = '' 
     for file_nm in sorted(glob.glob('../[!v]*/*.[c|h]')):
-        types, parsed = parse(file_nm) 
+        types, includes, parsed = parse(file_nm) 
+        DEPS[file_nm.split('/')[-1]] = set(includes)
         CODE += '\n\n'.join(texify(tag, data) for tag, data in parsed)
+        print(file_nm, includes)
         TYPES += types
-    print(TYPES)
+    print('FOUND TYPES:', ', '.join(TYPES))
+    print('FOUND DEPS:', DEPS)
 
     with open('template.tex') as f:
         tex = f.read()
     
     with open('out.tex', 'w') as f:
-        f.write(tex.replace('CODE', CODE).replace('TYPES', ','.join(TYPES)))
+        f.write(tex
+            .replace('CODE', CODE)
+            .replace('DEP_TABLE', make_dep_table(DEPS))
+            .replace('TYPES', ','.join(TYPES))
+        )
 
 if __name__=='__main__':
     do_all()
